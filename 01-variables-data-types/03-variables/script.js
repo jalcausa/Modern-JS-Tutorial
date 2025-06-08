5d8dc5f240d67b20a17ab67e199c9868f5cb43a7
Juan Carlos Alcausa Luque <jcalcausal@gmail.com>
2025-06-09 00:40:53 +0200

    3-Variables

--- 01-variables-data-types/03-variables/script.js
+++ 01-variables-data-types/03-variables/script.js
@@ -10,3 +10,17 @@
 // first_name    underscore
 // FirstName     PascalCase
 // firstname     owercase
+
+const arr = [1, 2, 3, 4, 5];
+arr.push(5);
+
+console.log(arr);
+
+//Se pueden declarar y asignar variables en la misma línea
+
+let a, b, c;
+
+console.log(a, b, c); // Sale undefined porque no están inicializadas
+
+let x =1, y=2, z=3;
+console.log(x, y, z);
