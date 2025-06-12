c905cb85189ae1118356be1988fb4abcdacdb420
Juan Carlos Alcausa Luque <jcalcausal@gmail.com>
2025-06-12 21:21:22 +0200

    Operadores

--- 01-variables-data-types/07-operators/script.js
+++ 01-variables-data-types/07-operators/script.js
@@ -1,7 +1,32 @@
 // 1. Arithmetic Operators
 
+let x;
+
+x = 5 + 5;
+x = 5 - 5;
+x = 5 * 5;
+x = 5 / 5;
+x = 7 % 5;
+
+// Concatenación
+
+x = "Hello " + "world";
+
+x = 1;
+x += 1;
 
 // 2. Assignment Operators
 
+x = 10;
+
+x *= 10;
 
 // 3. Comparison Operators
+
+x = 2 == 2
+
+x = 2 == "2"
+
+x = 2 !== 2
+
+console.log(x);
