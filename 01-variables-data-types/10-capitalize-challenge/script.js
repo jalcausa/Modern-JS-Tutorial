4aa088774e79ddcbe641975ce8d7231d0384f887
Juan Carlos Alcausa Luque <jcalcausal@gmail.com>
2025-06-15 23:59:55 +0200

    Capitalize challenge

--- 01-variables-data-types/10-capitalize-challenge/script.js
+++ 01-variables-data-types/10-capitalize-challenge/script.js
@@ -1,2 +1,10 @@
 // Create a new string called "myNewString" that holds the value of "Developer", using the value from "myString"
 const myString = 'developer';
+
+let firstLetter = myString.charAt(0).toUpperCase();
+
+let tail = myString.substring(1);
+
+let res = firstLetter + tail;
+
+console.log(res);
