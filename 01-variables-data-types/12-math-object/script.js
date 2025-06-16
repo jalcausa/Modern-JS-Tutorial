4a3b6ac01a9950844ba529f522e9ba6efd8ce598
Juan Carlos Alcausa Luque <jcalcausal@gmail.com>
2025-06-16 23:12:46 +0200

    Math object

---
+++ 01-variables-data-types/12-math-object/script.js
@@ -0,0 +1,25 @@
+let x;
+
+x = Math.sqrt(9);
+
+x = Math.abs(-5);
+
+x = Math.round(4.2); // 4
+
+x = Math.ceil(4.2); // 5
+
+x = Math.floor(4.9); // 4
+
+x = Math.pow (2, 3); // 8
+
+x = Math.min(4, 5, 3); // 3
+
+x = Math.max(4, 5, 3); // 5
+
+x = Math.random(); // random number in [0,1)
+
+x = Math.floor(Math.random() * 10); // random number in [0, 9]
+
+x = Math.floor(Math.random() * 100 + 1); // random number in [0, 100]
+
+console.log(x);
