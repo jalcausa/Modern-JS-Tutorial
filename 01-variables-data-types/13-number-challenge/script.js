a5cdee25d84225741fa496025e7266944c3e37e0
Juan Carlos Alcausa Luque <jcalcausal@gmail.com>
2025-06-16 23:19:09 +0200

    Number challeng

---
+++ 01-variables-data-types/13-number-challenge/script.js
@@ -0,0 +1,9 @@
+let x = Math.floor(Math.random() * 100 + 1);
+
+let y = Math.floor(Math.random() * 50 + 1);
+
+console.log(`Sum of ${x} and ${y} = ${x + y}`);
+console.log(`Difference of ${x} and ${y} = ${x - y}`);
+console.log(`Product of ${x} and ${y} = ${x * y}`);
+console.log(`Quotient of ${x} and ${y} = ${x / y}`);
+console.log(`Remainder of ${x} and ${y} = ${x % y}`);
