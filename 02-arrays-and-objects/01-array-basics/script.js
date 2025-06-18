72ff5141b0668413742b9aa8a4397e100522584f
Juan Carlos Alcausa Luque <jcalcausal@gmail.com>
2025-06-18 12:56:58 +0200

    Array-basics

---
+++ 02-arrays-and-objects/01-array-basics/script.js
@@ -0,0 +1,22 @@
+let x;
+
+// Array literal
+const numbers = [12, 45, 33, 29, 39];
+
+// Array Constructor
+const fruits = new Array('apple', 'grape', 'orange');
+const mixed = [1, "hola", null, undefined];
+
+x = numbers[0];
+
+x = fruits[0];
+
+x = fruits.length;
+
+x = mixed[0];
+
+fruits[2] = 'pear';
+
+fruits[fruits.length + 1] = 'peach';
+
+console.log(fruits[3]);
