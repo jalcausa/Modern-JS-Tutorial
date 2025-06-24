6526c470dec9a7c0a5ae0cb5cadb8172a07687a8
Juan Carlos Alcausa Luque <jcalcausal@gmail.com>
2025-06-24 14:20:05 +0200

    Execution context in action

--- 03-functions-scope/10-execution-context-in-action/script.js
+++ 03-functions-scope/10-execution-context-in-action/script.js
@@ -14,7 +14,7 @@
 1. Se crea el objeto global (en el navegador es el window, en Node.js el global).
 2. Se crea el objeto this y se le asocia el objeto global.
 3. Se configura el heap para almacenar variables y referencias a funciones (parte izquierda de la imagen)
-4. Se almacenan las funciones y variables en el contexto global de ejecución y se les asocia undefined (en la imagen salen ya con valores asociados pero en este punto estarían todos como undefined)
+4. Se almacenan las funciones y variables en el contexto global de ejecución PERO no son accesibles aún  (en el vídeo dice que se les asocia undefined a las variables pero realmente creo que no es a´si, están en la TDZ (Temporal Dead Zone) en la que existen pero no se pueden acceder) y el código que ejecuta cada función a las funciones (en la imagen salen ya con valores asociados a las variables pero en este punto estarían todas como undefined)
 
 
 2) Fase de ejecución
@@ -28,3 +28,21 @@
 
 /*------------------------- Contexto de ejecución en acción ----------------------------*/
 
+const x = 100;
+const y = 50;
+
+function getSum(n1, n2) {
+	const sum = n1 + n2;
+	return sum;
+}
+
+const sum1 = getSum(x, y);
+const sum2 = getSum(10, 5);
+
+/*
+
+Desde las herramientas para desarrolladores de Chrome podemos darle a sources para ver el código y crear breakpoints pulsando en una línea para que el código se detenga ahí.
+
+Podemos ir viendo como se crean los contextos de ejecución en la parte de scope y cómo al llamar a una función se crea una zona llamada local que representa el contexto de ejecución de esa función provocada por la llamada
+
+*/
