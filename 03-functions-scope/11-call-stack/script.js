a5aba70a6c746045bf7297be986b659d96b5e64c
Juan Carlos Alcausa Luque <jcalcausal@gmail.com>
2025-06-25 18:30:43 +0200

    Functions scope finished

---
+++ 03-functions-scope/11-call-stack/script.js
@@ -0,0 +1,12 @@
+/*------------------- LA PILA DE LLAMADAS --------------------- */
+/*
+
+- Pila de funciones a ejecutar (una por cada llamada a función)
+
+- Gestiona los contextos de ejecución de cada una de ellas
+
+- Las pilas son LIFO (Last In First Out)
+
+- Desde las herramientas para desarrolladores de Chrome podemos ver la pila de llamadas a función desde sources.
+
+*/
