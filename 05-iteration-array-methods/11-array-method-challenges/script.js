e8cac42bdfd7b4ecb04fb6fb33018ae8eb4611d8
Juan Carlos Alcausa Luque <jcalcausal@gmail.com>
2025-07-07 11:55:11 +0200

    Arrays methods challenge

--- 05-iteration-array-methods/11-array-method-challenges/script.js
+++ 05-iteration-array-methods/11-array-method-challenges/script.js
@@ -37,5 +37,23 @@ const people = [
   },
 ];
 
+/*
+Take the people array and create an array called youngPeople that stores objects with ONLY name and email properties of all the people that are 25 and under. The name property should have their first and last name.
+*/
+
+const youngPeople = people.filter(p => p.age <= 25).map(p => ({
+	name: p.firstName + " " + p.lastName,
+	email: p.email
+}));
+
+console.log(youngPeople);
+
 // Challenge 2
 const numbers = [2, -30, 50, 20, -12, -9, 7];
+
+const positiveSum = numbers.filter(n => n >= 0).reduce((acc, cur) => acc + cur);
+console.log(positiveSum);
+
+const words = ["coder", "programmer", "developer"];
+const capitalizedWords = words.map(word => word.charAt(0).toUpperCase() + word.substring(1, word.length));
+console.log(capitalizedWords);
